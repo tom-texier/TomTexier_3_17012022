ac3580dd8638dee541279e59c02710f9e4740de4
Tom Texier <tom.texierpro@gmail.com>
2022-01-25 12:59:49 +0100

    Correction Notre Programme - position: sticky

--- public/js/programme.js
+++ public/js/programme.js
@@ -1,20 +1,20 @@
-const sectionProgramme = document.querySelector('section.programme');
-const backgroundProgramme = sectionProgramme.querySelector('.background');
+// const sectionProgramme = document.querySelector('section.programme');
+// const backgroundProgramme = sectionProgramme.querySelector('.background');
 
-window.addEventListener('scroll', function() {
-    const marginTop = (window.getComputedStyle(sectionProgramme).marginTop).replace('px', '');
-    if(sectionProgramme.getBoundingClientRect().top <= marginTop) {
-        if((sectionProgramme.offsetTop + sectionProgramme.clientHeight - backgroundProgramme.clientHeight + parseInt(marginTop)) < window.scrollY) {
-            sectionProgramme.querySelector('.background').classList.remove('fixed');
-            sectionProgramme.querySelector('.background').classList.add('bottom');
-        }
-        else {
-            sectionProgramme.querySelector('.background').classList.add('fixed');
-            sectionProgramme.querySelector('.background').classList.remove('bottom');
-        }
-    }
-    else {
-        sectionProgramme.querySelector('.background').classList.remove('fixed');
-        sectionProgramme.querySelector('.background').classList.remove('bottom');
-    }
-});
+// window.addEventListener('scroll', function() {
+//     const marginTop = (window.getComputedStyle(sectionProgramme).marginTop).replace('px', '');
+//     if(sectionProgramme.getBoundingClientRect().top <= marginTop) {
+//         if((sectionProgramme.offsetTop + sectionProgramme.clientHeight - backgroundProgramme.clientHeight + parseInt(marginTop)) < window.scrollY) {
+//             sectionProgramme.querySelector('.background').classList.remove('fixed');
+//             sectionProgramme.querySelector('.background').classList.add('bottom');
+//         }
+//         else {
+//             sectionProgramme.querySelector('.background').classList.add('fixed');
+//             sectionProgramme.querySelector('.background').classList.remove('bottom');
+//         }
+//     }
+//     else {
+//         sectionProgramme.querySelector('.background').classList.remove('fixed');
+//         sectionProgramme.querySelector('.background').classList.remove('bottom');
+//     }
+// });
